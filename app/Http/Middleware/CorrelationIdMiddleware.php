f724d6794b324146acf968dedad54b802c67b932
Wescley Matos <wescleymatos@gmail.com>
2024-08-08 03:27:43 +0100

    implementando pipeline

--- app/Http/Middleware/CorrelationIdMiddleware.php
+++ app/Http/Middleware/CorrelationIdMiddleware.php
@@ -20,25 +20,18 @@ class CorrelationIdMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $correlationId = $request->header('X-Correlation-Id', Uuid::uuid4()->toString());
-        $traceId = bin2hex(random_bytes(8));
-        $spanId = bin2hex(random_bytes(8));
 
-        $this->logger->trace($traceId, 'trace start {traceId}', [
-            'traceId' => $traceId
+        $this->logger->info('[CorrelationIdMiddleware] - {correlationId} - Iniciar Middleware', [
+            'correlationId' => $correlationId
         ]);
-
-        $this->logger->span($traceId, $spanId, "Span {spanId} ended within trace {traceId}.",  ['traceId' => $traceId, 'spanId' => $spanId, '@tr' => $traceId, '@sp' => $spanId]);
 
         $request->attributes->set('correlationId', $correlationId);
 
         $response = $next($request);
         $response->headers->set('X-Correlation-Id', $correlationId);
 
-        $this->logger->span($traceId, $spanId, "Span {spanId} started within trace {traceId}.",  ['traceId' => $traceId, 'spanId' => $spanId, '@tr' => $traceId, '@sp' => $spanId]);
-        //$this->logger->span($traceId, $spanId, '[CorrelationIdMiddleware] - span');
-
-        $this->logger->trace($traceId, 'trace end {traceId}', [
-            'traceId' => $traceId
+        $this->logger->info('[CorrelationIdMiddleware] - {correlationId} - Finalizar Middleware', [
+            'correlationId' => $correlationId
         ]);
 
         return $response;
